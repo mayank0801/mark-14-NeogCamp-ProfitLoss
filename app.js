896b7bf4139010af5033327467b27ca5f6fec4ce
mayank0801 <36763315+mayank0801@users.noreply.github.com>
2022-10-18 23:44:15 +0530

    docs: added files from vs code

---
+++ app.js
@@ -0,0 +1,53 @@
+var IntailPrice=document.querySelector("#intial-Price");
+var Qnty=document.querySelector("#Qntatity-Stock");
+var currentPrice=document.querySelector("#current-Price");
+var btnCheck=document.querySelector("#btn-Check");
+var output=document.querySelector("#output-Msg");
+
+
+btnCheck.addEventListener("click",checkStock);
+
+function checkStock(){
+    // console.log("clicked");
+    var initialPrc=IntailPrice.value;
+    var Qnt=Qnty.value;
+    var currentPrc=currentPrice.value;
+    if(initialPrc===""||Qnt===""||currentPrc===""){
+        // alert("Please Enter all required Field");
+        output.innertext="Please Enter all required Field";
+        return;
+    }
+    else if(initialPrc<0||Qnt<0||currentPrc<0){
+        output.innerText="Input can't be Negative";
+        return;
+    }
+    calculateProfitAndLoss(initialPrc,Qnt,currentPrc);
+
+}
+
+
+function calculateProfitAndLoss(intial,quantity,current){
+    if(current>intial){
+        var profit=(current-intial)*quantity;
+        var profitPer=((profit/(intial*quantity))*100).toFixed(2);
+        document.body.style.backgroundColor ="green";
+        errorMessage(`Your Profit is ${profit} and Profit Percentage is ${profitPer}`)
+    }
+    else if(intial>current)
+    {
+        var loss=(intial-current)*quantity;
+        var lossPer=((loss/(intial*quantity))*100).toFixed(2);
+        document.body.style.backgroundColor="red";
+        errorMessage(`Your Loss is ${loss} and Loss Percentage is ${lossPer}`)
+    }
+    else{
+        errorMessage(`No pain No gain and No gain No pain`);
+    }
+}
+
+function errorMessage(message,profit){
+    output.innerHTML=message;
+}
+
+
+
